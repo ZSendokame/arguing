c0b5d8b4842b79653f8155b31cda2719a7675ad5
ZSendokame <sendokame@sendokame.netlify.com>
2022-05-21 15:05:24 -0400

    Changed version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ readme = open('README.md')
 
 setup(
     name='arguing',
-    version='1.0.4',
+    version='1.0.5',
     author='Sendokame',
     description='Create nice CLIs without bloating your apps.',
     long_description_content_type='text/markdown',
