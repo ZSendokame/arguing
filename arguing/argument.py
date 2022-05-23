ad776e4668f8e09a58333ef31d3a38d9dadd126c
ZSendokame <sendokame@sendokame.netlify.com>
2022-05-23 01:42:05 -0400

    Functional mandatory arguments.

--- arguing/argument.py
+++ arguing/argument.py
@@ -3,12 +3,18 @@ import sys
 argv = sys.argv
 argumentDict = {}
 
-def set(argument, argumentType=str):
-    argumentDict[argument] = argumentType
+def set(argument, argumentType=str, help=None):
+    argumentDict[argument] = {
+        'type': argumentType,
+        'help': help
+    }
+
+    if not argument in argv:
+        raise Exception(f'Mandatory argument "{argument}" cannot be founded.\n{argument}: {help}')
 
 def get(argument):
-    if argument in argv and not argument == sys.argv[-1]:
-        argumentValue = sys.argv[sys.argv.index(argument) + 1]
+    if argument in argv and not argument == argv[-1]:
+        argumentValue = argv[argv.index(argument) + 1]
 
     else:
         argumentValue = None
@@ -19,4 +25,16 @@ def get(argument):
     return argumentValue
 
 def check(argument):
-    return argument in sys.argv
+    return argument in argv
+
+def help():
+    helpMessage = ''
+    helpMessage += f'USAGE: python {argv[0]} -h\n'
+
+    for argument in argumentDict:
+        helpMessage += f'{argument}: {argumentDict[argument]["help"]}'
+
+        if not argument == list(argumentDict)[-1]:
+            helpMessage += '\n'
+
+    return helpMessage
