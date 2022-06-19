902024e047b381027289d7625facef48127b4d22
ZSendokame <sendokame@sendokame.netlify.com>
2022-06-19 15:07:00 -0400

    Metadata.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ readme = open('README.md')
 
 setup(
     name='arguing',
-    version='1.0.9',
+    version='1.1.0',
     author='Sendokame',
     url='https://github.com/ZSendokame/arguing',
     description='Create nice CLIs without bloating your apps.',
