e768d85ef3a4f800a29c07c85f68bc3316890ab6
Sendo kame <sendo@sendokame.com>
2022-07-22 10:05:31 -0400

    Change version, error fixed (1.3.3).

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ readme = open('README.md')
 
 setup(
     name='arguing',
-    version='1.3.2',
+    version='1.3.3',
     author='Sendokame',
     url='https://github.com/ZSendokame/arguing',
     description='Create nice CLIs without bloating your apps.',
