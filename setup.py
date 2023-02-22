9dd4f316faa6a816fdb152dd1dc907477645befe
ZSendokame <sendo@sendokame.com>
2023-02-22 09:17:48 -0300

    Metadata.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ readme = open('README.md')
 
 setup(
     name='arguing',
-    version='1.3.7',
+    version='1.3.8',
     author='Sendokame',
     url='https://github.com/ZSendokame/arguing',
     description='Create nice CLIs without bloating your apps.',
